c61fa1cb78a70ca7a86b0e8bb9097332ccf52ec1
Alikondie <alikondie@gmail.com>
2019-02-15 12:18:02 +0100

    Min Max Slider
    Basic min max Slider

---
+++ MoveAvatarEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(base_class))]
+class MinMaxSlider : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base_class avatar = (base_class)target;
+        base.OnInspectorGUI();
+        
+        GUILayout.Label("Angles",EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Min Val:", avatar._minSlope.ToString());
+        EditorGUILayout.LabelField("Max Val:", avatar._maxSlope.ToString());
+        EditorGUILayout.MinMaxSlider("SteepAscent",ref avatar._minSlope, ref avatar._maxSlope, 0.0f, 90.0f);
+
+    }
+
+}
